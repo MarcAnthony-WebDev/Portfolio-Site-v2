e61c0083fb8e2fde0213e66f1ada9483013bfe04
MarcAnthony-WebDev <marcanthony.webdev@gmail.com>
2024-10-29 19:47:26 -0400

    changes to nav

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import React from "react";
-import { useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
+
 import NavLinks from "./NavLinks";
 import Image from "next/image";
 import Link from "next/link";
@@ -10,14 +10,36 @@ import { Menu } from "lucide-react";
 
 const NavBar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [isFixed, setIsFixed] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.innerWidth < 1000) {
+        setIsFixed(window.scrollY > 100);
+      } else {
+        setIsFixed(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    window.addEventListener("resize", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+      window.removeEventListener("resize", handleScroll);
+    };
+  }, []);
 
   const closeMenu = () => setIsOpen(false);
 
   return (
     <div className="nav-container relative col-span-1 row-span-2 h-full bg-light-bg outline outline-1 outline-neutral-200 dark:bg-gray-900 dark:outline-neutral-700 md:min-h-full">
-      <aside className="top-0 flex h-max p-4 md:fixed md:p-5">
+      <div className={`${isFixed ? "h-48" : "h-0"}`}></div>
+      <aside
+        className={`md:p-5" top-0 flex h-max p-2 transition duration-200 md:fixed ${isFixed ? "fixed left-0 right-0 top-0 z-10 w-full bg-neutral-100 shadow-md dark:bg-slate-700" : "relative"}`}
+      >
         <nav className="nav flex w-full items-center justify-between md:h-full md:flex-col md:items-start md:gap-10">
-          <div className="image-container h-24 w-24 md:h-40 md:w-40">
+          <div className="image-container h-20 w-20 md:h-40 md:w-40">
             <Link href="/" passHref className="h-full w-full">
               <Image
                 src="/images/cropped-headshot.png"
