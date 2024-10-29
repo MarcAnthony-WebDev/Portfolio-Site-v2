9f47a15f07a7fddb82b2470ff7f5dc2d65e5569c
MarcAnthony-WebDev <marcanthony.webdev@gmail.com>
2024-10-29 17:38:23 -0400

    added headshot image

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import { useState } from "react";
 import NavLinks from "./NavLinks";
 import Image from "next/image";
+import Link from "next/link";
 
 import { Menu } from "lucide-react";
 
@@ -16,8 +17,16 @@ const NavBar = () => {
     <div className="nav-container relative col-span-1 row-span-2 h-full bg-light-bg outline outline-1 outline-neutral-200 dark:bg-gray-900 dark:outline-neutral-700 md:min-h-full">
       <aside className="top-0 flex h-max p-4 md:fixed md:p-5">
         <nav className="nav flex w-full items-center justify-between md:h-full md:flex-col md:items-start md:gap-10">
-          <div className="image-container">
-            <div className="img h-20 w-20 rounded-full bg-blue-700"></div>
+          <div className="image-container h-24 w-24 md:h-40 md:w-40">
+            <Link href="/" passHref className="h-full w-full">
+              <Image
+                src="/images/cropped-headshot.png"
+                alt="headshot photo of me, Marcanthony!"
+                width={1080}
+                height={1080}
+                className="img h-full w-full rounded-full shadow-sm"
+              />
+            </Link>
           </div>
           <button
             className={`sm:hidden ${isOpen ? "hidden" : "block"}`}
