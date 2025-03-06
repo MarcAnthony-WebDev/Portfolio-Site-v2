45306b4f5258d1d578ec864751b723488a44bb4a
MarcAnthony-WebDev <marcanthony.webdev@gmail.com>
2025-03-06 13:03:08 -0500

    fixed typing for Logos

--- components/Brands.tsx
+++ components/Brands.tsx
@@ -2,12 +2,17 @@ import React from "react";
 
 import { Logos } from "@/components"
 
+type LogoType = {
+  label:string;
+  icon: React.ReactElement;
+}
+
 const Brands = () => {
   return (
     <>
       <div className="h-10 brands-container w-52 xs:w-[350px] md:w-[500px]">
         <div className="brands-wrapper">
-          {Logos.concat(Logos).map((logo, index) =>(
+          {Logos.concat(Logos).map((logo:LogoType, index:number) =>(
             <div key={index} className="flex items-center gap-2">
               {logo.icon}
               <span className="text-sm">{logo.label}</span>
